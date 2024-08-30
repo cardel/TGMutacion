49ef0a46365ce14be74f483d9c8b49971cf45715
HaroldMontanoHurtado <harold.montano@correounivalle.edu.co>
2024-08-30 00:41:27 -0500

    remove a few tests

--- one_conf.py
+++ one_conf.py
@@ -1,9 +1,7 @@
-
-def isGreater(a, b):
-    return a > b
-
+# 
 def isEq(a, b):
     return a == b
 
 def rest(a, b):
     return a - b
+
--- test/test_security.py
+++ test/test_security.py
@@ -1,14 +1,12 @@
 from unittest import TestCase
-from one_conf import isGreater, isEq, rest
+from one_conf import isEq, rest
 
 class TestDatabaseFunctions(TestCase):
-    
-    def test_isGreater(self):
-        self.assertEqual(isGreater(5, 2), True)
     
     def test_isEq(self):
         self.assertEqual(isEq(5, 2), False)
     
     def test_rest(self):
         self.assertEqual(rest(5, 2), 3)
+    
     
