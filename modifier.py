579f0c8a20f56bf3cff1625e3419c2b9934e3969
HaroldMontanoHurtado <harold.montano@correounivalle.edu.co>
2024-09-12 22:19:38 -0500

    now, modifier.py allows search variables, Using the AST module

--- modifier.py
+++ modifier.py
@@ -1,16 +1,47 @@
-import pdb
+import ast
+import re
 
 
-def bubblesort(elms):
-    for n in range(len(elms) - 1, 0, -1):
-        for i in range(n):
-            if elms[i] > elms[i + 1]:  # error a proposito para el debug
-                elms[i], elms[i + 1] = elms[i + 1], elms[i]
+class WordFinder(ast.NodeVisitor):
+    def __init__(self, target_word):
+        self.target_word = target_word
+        self.found_lines = []
+        self.variable_values = {}
+
+    def visit_Str(self, node):
+        if re.search(self.target_word, node.s):
+            self.found_lines.append(node.lineno)
+        self.generic_visit(node)
+
+    def visit_Name(self, node):
+        if re.search(self.target_word, node.id):
+            self.found_lines.append(node.lineno)
+            # Intentar obtener el valor de la variable
+            try:
+                value = eval(node.id, globals(), locals())
+                self.variable_values[node.id] = value
+            except NameError:
+                self.variable_values[node.id] = 'Valor no disponible'
+        self.generic_visit(node)
 
 
-elements = [39, 12, 18, 85, 72, 10, 2, 18]
+def find_word_in_file(file_path, target_word):
+    with open(file_path, 'r') as file:
+        file_content = file.read()
 
-print(f'Lista sin ordenar: {elements}')
-#pdb.set_trace()  # breakpoint. Es permitido tener muchos (indefinidos).
-bubblesort(elements)
-print(f'Lista ordenada: {elements}')
+    tree = ast.parse(file_content)
+    finder = WordFinder(target_word)
+    finder.visit(tree)
+
+    return finder.found_lines, finder.variable_values
+
+
+# Ejemplo de uso
+file_path = 'configuration/configuration.py'
+target_word = 'SUPPORTS_CREDENTIALS'
+lines, variable_values = find_word_in_file(file_path, target_word)
+if lines:
+    print(f'La palabra "{target_word}" fue encontrada en las siguientes líneas: {lines}')
+    print(f'Valores de las variables encontradas: {variable_values}')
+else:
+    print(f'La palabra "{target_word}" no fue encontrada en el archivo.')
